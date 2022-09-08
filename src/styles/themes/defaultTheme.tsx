3ff320edd0bac76ef7bc20b69f77d6516b2a5e6e
amirrezaDev1378 <rezaamir731@gmail.com>
2022-09-08 17:33:13 +0430

    theme edited based on figma design

--- src/styles/themes/defaultTheme.tsx
+++ src/styles/themes/defaultTheme.tsx
@@ -1,13 +1,32 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import {createTheme} from '@mui/material/styles';
+import {red} from '@mui/material/colors';
 
-// Create a theme instance.
 const theme = createTheme({
     palette:
-    {
-        primary: { main: '#556cd6', },
-        secondary: { main: '#19857b', },
-        error: { main: red.A400, },
-    },
+        {
+            primary: {
+                main: '#4339F2',
+                light:"#DAD7FE"
+            },
+            secondary: {
+                main: 'rgb(88,108,166)',
+            },
+            error: {
+                main: "#FF3A29",
+                light:"#FFE5D3"
+            },
+            info:{
+                main: '#02A0FC',
+                light:"#CCF8FE"
+            },
+            warning:{
+                main:"#FFB200",
+                light:"#FFF5CC"
+            },
+            success:{
+                main:"#34B53A",
+                light:"#E2FBD7"
+            }
+        },
 });
 export default theme;

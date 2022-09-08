210b86b7c0b9422343bec24f803d9d65e779a572
amirrezaDev1378 <rezaamir731@gmail.com>
2022-09-08 17:33:28 +0430

    edited reusable form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,45 +1,40 @@
 import React, {FC} from 'react';
 import styles from "./Form.module.scss";
 import {useForm} from "react-hook-form";
-import {JSXElement} from "@babel/types";
 import {yupResolver} from "@hookform/resolvers/yup";
-import {Grid, Stack} from "@mui/material";
+import {Grid} from "@mui/material";
 import FormChildren from "@components/Form/FormChildren";
-import {ToastContainer, toast} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 export type FormConfig = {
     showError: boolean
 }
+export  type formSubmitHandlerType = (data: any , e:React.ChangeEvent<HTMLFormElement>) => void;
 
-export interface FormProps {
+export interface FormProps extends Omit<React.FormHTMLAttributes<HTMLFormElement>, "onSubmit"> {
     children: React.ReactNode[] | React.ReactNode;
     onSubmit: (data, e) => void,
     schema: any;
-    config?: FormConfig
+    config?: FormConfig,
+
 }
 
 
-const Form: FC<FormProps> = ({onSubmit, children, schema, ...props}) => {
-    const {register,control, handleSubmit, watch, formState: {errors, isSubmitting, isSubmitSuccessful }} = useForm({
+const Form: FC<FormProps> = ({onSubmit, children, schema, config, ...otherProps}) => {
+    const {register, control, handleSubmit, watch, formState: {errors, isSubmitting, isSubmitSuccessful}} = useForm({
         mode: "all",
         resolver: yupResolver(schema)
     });
-    console.log({errors})
 
     const submitHandler = (e) => {
         e.preventDefault();
         handleSubmit(onSubmit)(e)
-        toast("submit succeeded", {
-            type: "success",
-            position: "bottom-left"
-        })
+
     }
     return (
-        <form onSubmit={submitHandler}>
-            <Grid xs={4} item container direction={"column"} gap={2}>
+        <form onSubmit={submitHandler} {...otherProps}>
+            <Grid xs={12} item container direction={"column"} gap={2}>
                 <FormChildren register={register} errors={errors} children={children}/>
-                <ToastContainer/>
             </Grid>
         </form>
     );
--- src/components/Form/FormError.tsx
+++ src/components/Form/FormError.tsx
@@ -1,11 +1,12 @@
 import React, {FC} from 'react';
 import {Alert} from "@mui/material";
+import {Warning} from "@mui/icons-material";
 type FormError ={
     error: string
 }
 const FormError:FC<FormError> = ({error}) => {
     return (
-        <Alert severity={"error"}>
+        <Alert icon={<Warning color={"error"} />} color={"error"} severity={"error"}>
             {error}
         </Alert>
     );

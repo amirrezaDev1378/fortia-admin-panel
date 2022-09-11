b27ee4b07e0ca670d5d8e9b9c4276d42b2c08c9b
amirrezaDev1378 <rezaamir731@gmail.com>
2022-09-11 17:08:09 +0430

    added server.ts for future

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ import Head from 'next/head'
 
 import styles from '../styles/Home.module.css'
 import {Button} from "@mui/material";
+import BankCard from "@components/BankCard/BankCard";
+import {BabyChangingStation} from "@mui/icons-material";
 
 const IndexPage: NextPage = () => {
     return (
         <div className={styles.container}>
-            <Button>
-                mame
-            </Button>
+            <BankCard date={"22fds"} number={"sfdf"} color={"red"} name={"dfdsafs"} icon={<BabyChangingStation />} title={"dsds"} />
         </div>
     )
 }

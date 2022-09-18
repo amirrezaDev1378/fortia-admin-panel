2462fd6a204738201ec2a48542b254af111b10f9
amirrezaDev1378 <rezaamir731@gmail.com>
2022-09-18 19:06:16 +0430

    ...

--- src/containers/Dashboards/Financial/Financial.tsx
+++ src/containers/Dashboards/Financial/Financial.tsx
@@ -4,10 +4,12 @@ import DashboardContainer from "@containers/layout/DashboardContainer/DashboardC
 import {Grid, Stack} from "@mui/material";
 import BankCard from "@components/BankCard/BankCard";
 import {BsBank2} from "react-icons/all";
-import {getCards} from "@/services/getCards";
+import {getCards} from "@/services/cards/getCards";
 import {For, Show} from 'react-haiku';
 import Errors from "@containers/Dashboards/Financial/errors";
-
+import CreateBankCard from "@components/CreateBankCard/CreateBankCard";
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 export interface FinancialProps {
 
 }
@@ -16,29 +18,44 @@ const Financial: FC<FinancialProps> = (props) => {
 
     const {data, isLoading, error, hasError, hasNoCards} = getCards();
 
-    console.log({data, isLoading, error, hasError, hasNoCards})
+    const [ref] = useKeenSlider<HTMLDivElement>({
+        slides: {
+            perView: 2,
+            spacing: 20,
+            origin:"center"
+        }
+
+    })
 
     return (
         <DashboardContainer>
             <Show>
-                <Show.When isTrue={isLoading || hasNoCards || hasNoCards}>
+                <Show.When isTrue={isLoading || hasError || hasNoCards}>
                     <Errors isLoading={isLoading} hasError={hasError} hasNoCards={hasNoCards}/>
                 </Show.When>
 
+
                 <Show.Else>
-                    <Grid container item flexDirection={"row"} display={"flex"} gap={1}>
-                        <For each={data} render={(item, i) => {
-                            return <BankCard
-                                key={i}
-                                date={item.expireDate}
-                                number={item.cardNumber}
-                                color={"red"}
-                                name={item.ownerName}
-                                icon={<BsBank2/>}
-                                title={item.bankName}
-                            />
-                        }}/>
+                    <Grid container className={styles.Financial}>
+                        <Grid xs={8} className={"keen-slider"} ref={ref} container item flexDirection={"row"} display={"flex"} gap={1} flexWrap={"nowrap"}>
+                            <For each={data} render={(item, i) => {
+                                return <BankCard
+                                    className={"keen-slider__slide"}
+                                    key={i}
+                                    date={item.expireDate}
+                                    number={item.cardNumber}
+                                    color={"red"}
+                                    name={item.ownerName}
+                                    icon={<BsBank2/>}
+                                    title={item.bankName}
+                                />
+                            }}/>
+                        </Grid>
+                        <Show.When isTrue={!hasError && !isLoading}>
+                            <CreateBankCard/>
+                        </Show.When>
                     </Grid>
+
                 </Show.Else>
             </Show>
 

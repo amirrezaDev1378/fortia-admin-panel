8ab52f13969885971478e0335578b70c6992b023
amirrezaDev1378 <rezaamir731@gmail.com>
2022-10-05 17:20:10 +0330

    edited routes according to strapi rest apis

--- src/config/server/server.ts
+++ src/config/server/server.ts
@@ -2,23 +2,47 @@ import {MakeServerConfig} from "@/config/server/makeServerConfig";
 
 let server = new MakeServerConfig({
     address: 'http://localhost',
-    port: 3000
+    port: 1337
 })
 
 server.setRoutes({
-    login: '/rest/loginUser',
-    register: '/register',
+    login: '/api/auth/local',
+    register: '/api/auth/local/register',
     cards: {
-        getCards: '/rest/getAllCards',
-        addCard: '/rest/createCard',
-        deleteCard: '/cards/delete',
-        updateCard: '/cards/update'
+        getAll: '/api/cards',
+        add: '/api/cards',
+        delete: '/api/cards/$ID$',
+        update: '/api/cards/$ID$'
     },
     credit: {
-        getCredit: '/credit',
-        withdraw: '/credit/withdraw',
-        charge: '/credit/charge'
+        getCredit: '/api/credits/getTotal',
+        withdraw: '/api/credits',
+        charge: '/api/credits',
+        getChartData: '/api/credits/getChartData'
     },
+    files: {
+        image: '/api/upload'
+    },
+    products: {
+        getAll: '/api/products',
+        add: '/api/products',
+        delete: '/api/products/$ID$',
+        update: '/api/products/$ID$',
+        getById: '/api/products/$ID$'
+    },
+    assets: {
+        getAll: '/api/assets',
+        charge: '/api/assets',
+        withdraw: '/api/assets'
+    },
+    articles: {
+        getAll: '/api/articles',
+        add: '/api/articles',
+        delete: '/api/articles/$ID$',
+        update: '/api/articles/$ID$',
+        getById: '/api/articles/$ID$'
+    }
+
 })
 
 export const ServerContext = server.generateContext();

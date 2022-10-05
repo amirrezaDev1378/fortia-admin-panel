637777ba814c0ee78d81302f570255df8614275e
amirrezaDev1378 <rezaamir731@gmail.com>
2022-10-05 17:21:25 +0330

    edited axiosInstance to authentication using jwt

--- src/config/adaptor/axios.ts
+++ src/config/adaptor/axios.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
 import {ServerConfig} from "@/config/server/server";
-console.log(ServerConfig.routes.baseRoute)
+
+const getCustomHeaders = () => {
+    let headers = {};
+    if (localStorage.getItem("jwt")) {
+        headers["Authorization"] = `Bearer ${localStorage.getItem("jwt")}`;
+    }
+    return headers;
+
+}
 export const axiosInstance = axios.create({
     baseURL: ServerConfig.routes.baseRoute,
     withCredentials: true,
+    headers: getCustomHeaders()
 })
+
+// TODO delete local storage on usage

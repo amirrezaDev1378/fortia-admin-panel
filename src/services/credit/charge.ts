02113f16bf68f82257b2e25bf61210247493da9c
amirrezaDev1378 <rezaamir731@gmail.com>
2022-10-05 17:22:08 +0330

    edited services according to strapi

---
+++ src/services/credit/charge.ts
@@ -0,0 +1,22 @@
+import {axiosInstance} from "@/config/adaptor/axios";
+import {ServerConfig} from "@/config/server/server";
+import {ApiCreditCredit} from "@/types/strapi";
+import {GetStrapiRequest} from "@utils/tsHelpers";
+
+export const charge = async (formData) => {
+    const url = ServerConfig.routes.credit.charge;
+    const data = await axiosInstance.post(url, {
+        data: {
+            type: 'charge',
+            amount: +formData.amount
+        }
+    }).catch((e) => {
+        return e;
+    })
+    if (data instanceof Error || data.status !== 200) {
+        throw new Error(data.message);
+    } else {
+        return data.data;
+    }
+
+}

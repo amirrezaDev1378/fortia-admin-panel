68df7a84a6ce6734d9520a62ab11e864f8e88b82
amirrezaDev1378 <rezaamir731@gmail.com>
2022-10-05 17:25:12 +0330

    added a new type `GetStrapiRequest`

--- src/utils/tsHelpers.ts
+++ src/utils/tsHelpers.ts
@@ -1,7 +1,11 @@
 import {styled} from "@mui/system";
 import Grid from "@mui/material/Grid";
 import {ReactNode} from "react";
+import {CollectionTypeSchema} from "@strapi/strapi";
 
 export type Overwrite<Base, Overrides> = Omit<Base, keyof Overrides> & Overrides;
 
 
+export type GetStrapiRequest<T extends CollectionTypeSchema> = {
+    data: T["attributes"];
+}

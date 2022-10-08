102f3171a27fe779303bd3ffd5f4cc1d29bd63a7
amirrezaDev1378 <rezaamir731@gmail.com>
2022-10-08 13:37:33 +0330

    enhanced ContainedPieChart component

--- src/components/ContainedPieChart/ContainedPieChart.stories.tsx
+++ src/components/ContainedPieChart/ContainedPieChart.stories.tsx
@@ -22,6 +22,6 @@ const dummyData = [
 
 ProductArg.args = {
     data: dummyData,
-    noLabel:true
+    useLabel:false
 
 };
--- src/components/ContainedPieChart/ContainedPieChart.tsx
+++ src/components/ContainedPieChart/ContainedPieChart.tsx
@@ -3,59 +3,60 @@ import styles from "./ContainedPieChart.module.scss";
 import {Cell, Pie, ResponsiveContainer, PieChart} from "recharts";
 
 export interface ContainedPieChartProps {
-    data: [{
+    data: {
         name: string,
         value: number,
         color?: string,
-    }],
-    noLabel?: boolean
+    }[],
+    useLabel?: boolean
 }
 
 const RADIAN = Math.PI / 180;
-const renderCustomLabel = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index}) => {
+const renderCustomLabel = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index}, label: string) => {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
     return (
         <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-            {`${(percent * 100).toFixed(0)}%`}
+            {`${label}`}
         </text>
     );
 }
-const ContainedPieChart: FC<ContainedPieChartProps> = ({data, noLabel}) => {
+const ContainedPieChart: FC<ContainedPieChartProps> = ({data, useLabel}) => {
     const defaultColors = ["#f4aa2e", "#e81212", "#4552ff", "#b562ff"]
+    const customLabel = (params) => {
+        return renderCustomLabel(params, data[params.index].name)
+    }
     return (
-        <ResponsiveContainer width={400} height={400}>
-            <PieChart width={400} height={400}>
-                <Pie
+        <PieChart width={400} height={400}>
+            <Pie
 
-                    data={data}
-                    cx="50%"
-                    cy="50%"
-                    labelLine={false}
-                    label={noLabel ? undefined : renderCustomLabel}
-                    outerRadius={80}
-                    fill="#8884d8"
-                    dataKey="value"
-                >
-                    {data.map(
-                        (item, index) => {
-                            const color = item.color || defaultColors[index % defaultColors.length]
-                            return (
-                                <Cell key={`cell-${index}`} fill={color}/>
-                            )
-                        }
-                    )}
-                </Pie>
-            </PieChart>
-        </ResponsiveContainer>
+                data={data}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={useLabel ? undefined : customLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="value"
+            >
+                {data.map(
+                    (item, index) => {
+                        const color = item.color || defaultColors[index % defaultColors.length]
+                        return (
+                            <Cell key={`cell-${index}`} fill={color}/>
+                        )
+                    }
+                )}
+            </Pie>
+        </PieChart>
     );
 };
 
 export default ContainedPieChart;
 
 ContainedPieChart.defaultProps = {
-    noLabel: false
+    useLabel: false
 }
 // TODO - add hover animation

6c12c9e4188c70019888f37820ab29768f41efbd
amirrezaDev1378 <rezaamir731@gmail.com>
2022-10-08 13:38:38 +0330

    reusable function for converting assets data to chart format

---
+++ src/services/assets/getAssetsChartData.ts
@@ -0,0 +1,25 @@
+import useSWR from 'swr'
+import {ServerConfig} from "@/config/server/server";
+import {ApiAssetAsset} from "@/types/strapi";
+import {swrFetcher} from "@/config/adaptor/swrFetcher";
+import {ALLOWED_COINS} from "@/config/coins";
+import {TypedSwrAdaptor} from "@/config/adaptor/typedSwrAdaptor";
+import {GetStrapiRequest, GetStrapiRequestArr} from "@utils/tsHelpers";
+import {convertToChart} from "@/services/assets/convertToChart";
+
+type AssetsResponse = ApiAssetAsset["attributes"][]
+
+export const getAssetsChartData = () => {
+    const url = ServerConfig.routes.assets.getAll;
+
+    const {data, error, mutate} = useSWR(url, TypedSwrAdaptor<AssetsResponse>)
+    const chartData = convertToChart(data)
+    return {
+        data: chartData,
+        isLoading: !error && !data,
+        hasError: !!error,
+        error,
+        hasNoCards: !error && data?.length === 0,
+        mutate
+    }
+}

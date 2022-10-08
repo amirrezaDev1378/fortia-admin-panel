2dcb14a2fc754c1b96f4a6a8ae7c518b7e35ff01
amirrezaDev1378 <rezaamir731@gmail.com>
2022-10-08 13:39:21 +0330

    updated menu update

--- src/containers/layout/DashboardContainer/getMenuItems.tsx
+++ src/containers/layout/DashboardContainer/getMenuItems.tsx
@@ -1,11 +1,18 @@
 import {item} from "@containers/layout/Menu/Menu";
-import {AiOutlineDashboard, BsTelephoneForward, FaMoneyCheckAlt} from "react-icons/all";
+import {AiOutlineDashboard, AiOutlineMessage, AiOutlineSetting, AiOutlineUser, BsBagCheck, BsDoorOpen, TbReportMoney} from "react-icons/all";
 
 export const getMenuItems = (): item[] => {
+    const size = 30;
     return [
-        {name: 'Dashboard', link: '/', icon: <AiOutlineDashboard size={40}/>},
-        {name: 'Financial', link: '/financial', icon: <FaMoneyCheckAlt size={40} />, isActive: true},
-        {name: 'Contact', link: '/contact', icon: <BsTelephoneForward size={40}/>},
+        {name: 'Dashboard', link: '/', icon: <AiOutlineDashboard size={size}/>},
+        {name: 'Financial', link: '/financial', icon: <TbReportMoney size={size}/>, isActive: true},
+        {name: "Conversations", link: "/messages", icon: <AiOutlineMessage size={size}/>},
+        {name: "Orders", link: "/orders", icon: <BsBagCheck size={size}/>},
+        {name:"Profile", link:"/profile", icon:<AiOutlineUser size={size}/>},
+
+
+        {name:"Settings", link:"/settings", icon:<AiOutlineSetting size={size}/> , position:"bottom"},
+        {name:"Logout", link:"/logout", icon:<BsDoorOpen size={size} /> , position:"bottom"}
     ]
 }
 

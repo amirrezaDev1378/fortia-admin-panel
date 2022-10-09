282a1f74bd5a994668b2ddf1096f19d4664271e4
amirrezaDev1378 <rezaamir731@gmail.com>
2022-10-09 13:11:46 +0330

    added Range helper

--- src/utils/tsHelpers.ts
+++ src/utils/tsHelpers.ts
@@ -1,6 +1,4 @@
-import {styled} from "@mui/system";
-import Grid from "@mui/material/Grid";
-import {ReactNode} from "react";
+
 import {CollectionTypeSchema} from "@strapi/strapi";
 
 export type Overwrite<Base, Overrides> = Omit<Base, keyof Overrides> & Overrides;
@@ -12,3 +10,11 @@ export type GetStrapiRequest<T extends CollectionTypeSchema> = {
 export type GetStrapiRequestArr<T extends CollectionTypeSchema> = {
     data: T["attributes"][]
 }
+
+type PrependNextNum<A extends Array<unknown>> = A['length'] extends infer T ? ((t: T, ...a: A) => void) extends ((...x: infer X) => void) ? X : never : never;
+
+type EnumerateInternal<A extends Array<unknown>, N extends number> = { 0: A, 1: EnumerateInternal<PrependNextNum<A>, N> }[N extends A['length'] ? 0 : 1];
+
+export type Enumerate<N extends number> = EnumerateInternal<[], N> extends (infer E)[] ? E : never;
+
+export type Range<FROM extends number, TO extends number> = Exclude<Enumerate<TO>, Enumerate<FROM>>;

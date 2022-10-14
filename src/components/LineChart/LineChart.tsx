6aede08386b160673a905d576c7f56ca8863e6b0
Amirreza hossein panah <rezaamir731@gmail.com>
2022-10-14 17:50:32 +0330

    enhanced for responsive design.

--- src/components/LineChart/LineChart.tsx
+++ src/components/LineChart/LineChart.tsx
@@ -1,24 +1,29 @@
-import React , {FC} from 'react';
-import {Line, ResponsiveContainer, LineChart as RechartLineChart, Tooltip , XAxis as RechartXAxis} from 'recharts';
+import React, {FC} from 'react';
+import {Line, ResponsiveContainer, LineChart as RechartLineChart, Tooltip, XAxis as RechartXAxis} from 'recharts';
 import styles from "./LineChart.module.scss";
+
 export interface LineChartProps {
     data: {
         name: string,
         value: number
     }[],
     tooltip?: boolean,
-    dot?:boolean,
-    XAxis?:{
-        dataKey:string
+    dot?: boolean,
+    XAxis?: {
+        dataKey: string
     }
+    className?: string
 }
-const LineChart:FC<LineChartProps> = ({data,tooltip , dot,XAxis}) => {
+
+const LineChart: FC<LineChartProps> = ({data, tooltip, dot, XAxis, className}) => {
+
+    const classNameProp = className || styles.LineChart;
     return (
-        <ResponsiveContainer width={300} height={300}>
-            <RechartLineChart width={300} height={100} data={data}>
-                <Line dot={dot} type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={2} />
-                {tooltip && <Tooltip />}
-                {XAxis && <RechartXAxis dataKey={XAxis.dataKey} />}
+        <ResponsiveContainer className={classNameProp}>
+            <RechartLineChart data={data}>
+                <Line dot={dot} type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={2}/>
+                {tooltip && <Tooltip/>}
+                {XAxis && <RechartXAxis dataKey={XAxis.dataKey}/>}
             </RechartLineChart>
         </ResponsiveContainer>
     );
@@ -26,7 +31,7 @@ const LineChart:FC<LineChartProps> = ({data,tooltip , dot,XAxis}) => {
 
 export default LineChart;
 
-LineChart.defaultProps={
-    tooltip:false,
-    dot:false
+LineChart.defaultProps = {
+    tooltip: false,
+    dot: false
 }
